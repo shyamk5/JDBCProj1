f813077f65c722cd683dfe50f29d291af0e200d1
Shyam Kumar <65010740+shyamk5@users.noreply.github.com>
2023-05-27 11:23:54 +0530

    Working with Large Objects

---
+++ JDBCProj1/src/com/nt/jdbc1/MatrimonyAppOracle.java
@@ -0,0 +1,104 @@
+package com.nt.jdbc1;
+
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.Reader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
+public class MatrimonyAppOracle {
+
+	private final static String MAPP_INSERT_QUERY = "INSERT INTO MATRIMONY_APP VALUES(MAPP_SEQ.NEXTVAL,?,?,?,?,?,?,?,?)";
+	
+	public static void main(String[] args) {
+		
+		try(Scanner scn = new Scanner(System.in)) {
+			
+			String cname = null;
+			String gender = null;
+			String dob = null;
+			String photoLocation = null;
+			String resumeLocation = null;
+			String biodataLocation = null;
+			String audioInfo = null;
+			String videoInfo = null;
+			
+			//read inputs
+			System.out.print("Enter Your Name: ");
+			cname = scn.next().toUpperCase();
+			System.out.print("Enter Your Gender(M/F): ");
+			gender = scn.next().toUpperCase();
+			System.out.print("Enter Your Date of Birth(dd-MM-YYYY): ");
+			dob = scn.next();
+			System.out.print("Enter Your Photo Location (file path): ");
+			photoLocation = scn.next().replace("?", "");
+			System.out.print("Enter Your Resume Location (file path): ");
+			resumeLocation = scn.next().replace("?", "");
+			System.out.print("Enter Your Biodata Location (file path): ");
+			biodataLocation = scn.next().replace("?", "");
+			System.out.print("Enter Your Fav. Audio Location (file path): ");
+			audioInfo = scn.next().replace("?", "");
+			System.out.print("Enter Your Fav. Video Location (file path): ");
+			videoInfo = scn.next().replace("?", "");
+			
+			//convert String date values to java.sql.Date class obj
+			SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-YYYY");
+			java.util.Date udob = sdf.parse(dob);
+			
+			//convert java.util.Date class obj to java.sql.Date class object
+			long ms = udob.getTime();
+			java.sql.Date sdob = new java.sql.Date(ms);
+			
+			try(InputStream isp = new FileInputStream(photoLocation);
+					Reader resumeReader = new FileReader(resumeLocation);
+					Reader biodataReader = new FileReader(biodataLocation);
+					Reader audioReader = new FileReader(audioInfo);
+					Reader videoReader = new FileReader(videoInfo);
+					) {
+				
+				try(Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","IMSHYAM","URVIL");
+						PreparedStatement ps = con.prepareStatement(MAPP_INSERT_QUERY);
+						) {
+					//set Query param values
+					if(ps != null) {
+						ps.setString(1, cname);
+						ps.setString(2, gender);
+						ps.setDate(3, sdob);
+						ps.setBinaryStream(4, isp);
+						ps.setCharacterStream(5, resumeReader);
+						ps.setCharacterStream(6, biodataReader);
+						ps.setCharacterStream(7, audioReader);
+						ps.setCharacterStream(8, videoReader);
+					}//if
+					
+					//execute the query
+					int count = 0;
+					if(ps != null)
+						count = ps.executeUpdate();
+					
+					if(count == 0)
+						System.out.println("Record not Inserted");
+					else
+						System.out.println("Record Inserted");
+
+				}//try
+			}//try
+		}//try
+		
+		catch(SQLException se) {
+			se.printStackTrace();
+			System.out.println("Problem in Record Insertion");
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+
+	}//main
+
+}//class
---
+++ JDBCProj1/src/com/nt/jdbc1/RetrieveBlobTest.java
@@ -0,0 +1,71 @@
+package com.nt.jdbc1;
+
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import org.apache.commons.io.IOUtils;
+
+public class RetrieveBlobTest {
+	
+	private final static String RETRIEVE_BLOB_QUERY = "SELECT AID,NAME,ADDRESS,PHOTO FROM ARTIST_INFO WHERE AID = ?";
+
+	public static void main(String[] args) {
+		
+		try(Scanner scn = new Scanner(System.in)) {
+			//read inputs
+			int aid = 0;
+			if(scn != null) {
+				System.out.print("Enter Artist Id: ");
+				aid = scn.nextInt();
+			}
+			//create Connection and PreparedStatement object
+			try(Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","IMSHYAM","URVIL");
+					PreparedStatement ps = con.prepareStatement(RETRIEVE_BLOB_QUERY);
+					) {
+				//set query param values
+				if(ps != null)
+					ps.setInt(1, aid);
+				
+				//execute query
+				try(ResultSet rs = ps.executeQuery()) {
+					//process the results
+					if(rs != null) {
+						if(rs.next()) {
+							int a_aid = rs.getInt(1);
+							String name = rs.getString(2);
+							String addrs = rs.getString(3);
+							System.out.println(a_aid+"\t"+name+"\t"+addrs);
+							//get InputStream pointing to BLOB col values
+							try(InputStream is = rs.getBinaryStream(4);
+									OutputStream os = new FileOutputStream("image_retrieve.jpg");
+									) {
+								//copy BLOB col value to destination file
+								IOUtils.copy(is,os);
+								System.out.println("BLOB value is retrieved and stored in the file");
+							}//try
+						}//if
+						else {
+							System.out.println("Record not found");
+						}
+					}//if
+				}//try
+				
+			}//try
+		}//try
+		catch(SQLException se) {
+			se.printStackTrace();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+
+	}//main
+
+}//class
