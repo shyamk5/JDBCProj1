24a22e74d8e11f3791f4001419d95387e7701c6e
Shyam Kumar <65010740+shyamk5@users.noreply.github.com>
2023-06-08 08:25:28 +0530

    Scrollable ResultSet

---
+++ JDBCProj1/src/com/nit/jdbc3/ScrollableRsTest.java
@@ -0,0 +1,75 @@
+package com.nit.jdbc3;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ScrollableRsTest {
+
+	private static final String EMP_SELECT_QUERY = "SELECT EMPNO,ENAME,JOB,SAL FROM EMP";
+	
+	public static void main(String[] args) {
+		
+		
+		try(Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","IMSHYAM","URVIL");
+				Statement st = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+																									//type										,					//mode
+				ResultSet rs = st.executeQuery(EMP_SELECT_QUERY);
+				) {
+			
+			if(rs != null) {
+				
+				System.out.println("RS records top to bottom");
+				while(rs.next()) {
+					System.out.println(rs.getRow()+"-------->"+rs.getInt(1)+"\t\t"+rs.getString(2)+"\t\t"+rs.getString(3)+"\t\t\t"+rs.getFloat(4));
+				}
+
+				System.out.println("=====================================");
+				System.out.println("RS record bottom to top");
+				rs.afterLast();
+				while(rs.previous()) {
+					System.out.println(rs.getRow()+"-------->"+rs.getInt(1)+"\t\t"+rs.getString(2)+"\t\t"+rs.getString(3)+"\t\t"+rs.getFloat(4));
+				}
+				
+				System.out.println("=====================================");
+				System.out.println("RS First record");
+				rs.first();
+				System.out.println(rs.getRow()+"-------->"+rs.getInt(1)+"\t\t"+rs.getString(2)+"\t\t"+rs.getString(3)+"\t\t"+rs.getFloat(4));
+				
+				System.out.println("=====================================");
+				System.out.println("RS Last record");
+				rs.last();
+				System.out.println(rs.getRow()+"-------->"+rs.getInt(1)+"\t\t"+rs.getString(2)+"\t\t"+rs.getString(3)+"\t\t"+rs.getFloat(4));
+				
+				System.out.println("=====================================");
+				System.out.println("RS - From the top 3rd record");
+				rs.absolute(3);
+				System.out.println(rs.getRow()+"-------->"+rs.getInt(1)+"\t\t"+rs.getString(2)+"\t\t"+rs.getString(3)+"\t\t"+rs.getFloat(4));
+				
+				System.out.println("=====================================");
+				System.out.println("RS - From the bottom 6th record");
+				rs.absolute(-6);
+				System.out.println(rs.getRow()+"-------->"+rs.getInt(1)+"\t\t"+rs.getString(2)+"\t\t"+rs.getString(3)+"\t\t"+rs.getFloat(4));
+				
+				System.out.println("=====================================");
+				System.out.println("RS - relative(4)");
+				rs.relative(4);
+				System.out.println(rs.getRow()+"-------->"+rs.getInt(1)+"\t\t"+rs.getString(2)+"\t\t"+rs.getString(3)+"\t\t"+rs.getFloat(4));
+				
+				System.out.println("=====================================");
+				System.out.println("RS - relative(-4)");
+				rs.relative(-4);
+				System.out.println(rs.getRow()+"-------->"+rs.getInt(1)+"\t\t"+rs.getString(2)+"\t\t"+rs.getString(3)+"\t\t"+rs.getFloat(4));
+				
+			}//if
+			
+			
+			
+		}//try
+		catch(SQLException se) {
+			se.printStackTrace();
+		}
+	}//main
+}//class
